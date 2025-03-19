ad24a7df7a17eee770c3c96ea786317aa9ad6a63
Joel Garay <joelgaray2020@gmail.com>
2025-03-19 17:07:48 -0500

    Agregar componente APITester para probar endpoints API en la aplicación

---
+++ src/frontend/App.tsx
@@ -0,0 +1,23 @@
+import type { FC } from 'react'
+import { APITester } from './APITester'
+import './index.css'
+
+import logo from './logo.svg'
+import reactLogo from './react.svg'
+
+export const App: FC = () => {
+  return (
+    <div className='app'>
+      <div className='logo-container'>
+        <img src={logo} alt='Bun Logo' className='logo bun-logo' />
+        <img src={reactLogo} alt='React Logo' className='logo react-logo' />
+      </div>
+
+      <h1>Bun + React</h1>
+      <p>
+        Editar <code>src/App.tsx</code> y guardar para probar HMR con Bun.
+      </p>
+      <APITester />
+    </div>
+  )
+}
